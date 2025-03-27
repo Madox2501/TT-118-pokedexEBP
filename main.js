c5c3b9b73dcd64c6f0d1f04bbc86a5b7da72513c
madox2501 <emerson.barona@gmail.com>
2025-03-27 06:59:02 -0500

    ”actualizacion_mar_27”

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const pokemonList = document.getElementById("pokemonList")
 const pokemonDetail = document.getElementById("pokemonDetail")
 const pokemonInfo = document.getElementById("pokemonInfo")
 const btnBack = document.getElementById("btnBack")
+const searchInput = document.getElementById("searchInput")
+const btnSearch = document.getElementById("btnSearch")
+let pokemonToSearch = ""
+
 
 //esta funcion llama el endpoint de la api
 async function getPokemonData(pokemonID) {
@@ -13,10 +17,13 @@ async function getPokemonData(pokemonID) {
         
     } catch (error) {
         console.error(error.message)
-        return null 
+        return false 
     }
 
 }
+
+
+//funcion para mostrar el pokemon en tarjeta
 function displayPokemon(pokemon){
     const pokemonCard = document.createElement("div")
     pokemonCard.classList.add("pokemon-card")
@@ -29,12 +36,17 @@ function displayPokemon(pokemon){
     pokemonList.appendChild(pokemonCard)
     return true
 }
+
+//funcion para mostrar la info del pokemon ocultando el listado principal
 function showPokemonDetail(pokemon){
-    console.log(pokemon.types.length)
-    let types =" "
+    console.log(pokemon)
+    showPokemonStats(pokemon)
+    let typesName =[]
+    let typesImg =""
     for(i=0;i<pokemon.types.length;i++){
         console.log(pokemon.types[i].type.name)
-        types = types + pokemon.types[i].type.name
+        typesImg = typesImg + `<img src="./assets/${pokemon.types[i].type.name}.svg" height="100px" alt="">`
+        typesName.push(pokemon.types[i].type.name)
     }
  
     pokemonList.style.display = "none"
@@ -42,18 +54,48 @@ function showPokemonDetail(pokemon){
     pokemonInfo.innerHTML =`
     <img src="${pokemon.sprites.front_default}" alt="image view front ${pokemon.name}">
     <img src="${pokemon.sprites.back_default}" alt="image view back ${pokemon.name}">
-    <h3>${pokemon.name}</h3>    
+    <h3>${pokemon.name}</h3>
+    <h3>${pokemon.id}</h3>
+    <div>${typesImg}</div>
+    ${showPokemonStats(pokemon)}
     `
 }
+
+//funcion para hacer la busqueda del listado de pokemones
 async function loadPokedex() {
     for (let i=1;i<=5;i++){
     let pokemon = await getPokemonData(i)
     displayPokemon(pokemon)
-    
     }
 }
 btnBack.addEventListener("click",()=>{
     pokemonList.style.display = "grid"
     pokemonDetail.style.display = "none"
 })
+
+searchInput.addEventListener("input",(e)=>{
+    pokemonToSearch = e.target.value
+    console.log(pokemonToSearch)
+})
+
+btnSearch.addEventListener("click",async ()=>{
+    let pokemon = await getPokemonData(pokemonToSearch)
+    if(pokemon==false){
+        console.error("pokemon not found")
+        return alert("pokemon not found")
+    }
+    showPokemonDetail(pokemon)
+})
+
+function showPokemonStats(pokemon){
+    console.log(pokemon)
+    let stats =[]
+    let base_stat =""
+    for(let i=0;i<pokemon.stats.length;i++){
+        console.log(pokemon.stats[i].base_stat)
+    }
+}
+
+
+
 loadPokedex()
